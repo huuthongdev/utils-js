e05362eaa69feec11430a7fa1f4862d98a88fff5
huuthongdev <39242935+huuthongdev@users.noreply.github.com>
2019-11-19 11:52:03 +0700

    update

--- utils/string.utils.js
+++ utils/string.utils.js
@@ -1,4 +1,4 @@
-export function ToSlug(title) {
+export function toSlug(title) {
     let slug;
     //Đổi chữ hoa thành chữ thường
     slug = title.toLowerCase();
@@ -28,11 +28,11 @@ export function ToSlug(title) {
     return slug;
 }
 
-export function SearchString(searchText, string) {
+export function searchString(searchText, string) {
     return convertToSearch(string).search(convertToSearch(searchText)) !== -1;
 }
 
-export function FormatPhoneNumber(tel) {
+export function formatPhoneNumber(tel) {
     var C = tel.replace(/[^0-9xX]/g, "").replace(/[xX]/g, "x");
     var B = "";
     if (C.indexOf("x") > -1) {
@@ -44,32 +44,38 @@ export function FormatPhoneNumber(tel) {
     return tel;
 }
 
-export const Capitalize = ([first, ...rest]) =>
+export const capitalize = ([first, ...rest]) =>
   first.toUpperCase() + rest.join('');
 // Ex:  
 // capitalize('fooBar'); // 'FooBar'
 // capitalize('fooBar', true); // 'FooBar'
 
-export const Decapitalize = ([first, ...rest]) =>
+export const decapitalize = ([first, ...rest]) =>
   first.toLowerCase() + rest.join('')
 // Ex:
 // decapitalize('FooBar'); // 'fooBar'
 // decapitalize('FooBar'); // 'fooBar'
 
-export const CapitalizeEveryWord = str => str.replace(/\b[a-z]/g, char => char.toUpperCase());
+export const capitalizeEveryWord = str => str.replace(/\b[a-z]/g, char => char.toUpperCase());
 // Ex:
 // capitalizeEveryWord('hello world!'); // 'Hello World!'
 
-export function StripHTMLTags(str) {
+export function stripHTMLTags(str) {
     return str.replace(/<[^>]*>/g, '');
 }
 
-export const AverageBy = (arr, fn) => arr.map(typeof fn === 'function' ? fn : val => val[fn]).reduce((acc, val) => acc + val, 0) / arr.length;
+export const averageBy = (arr, fn) => arr.map(typeof fn === 'function' ? fn : val => val[fn]).reduce((acc, val) => acc + val, 0) / arr.length;
 // Ex:
 // averageBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], o => o.n); // 5
 // averageBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], 'n'); // 5
 
-export function IsPhoneNumber(phone) {
+export function isPhoneNumber(phone) {
     if (!phone) return false;
     return /^(\+?84|0)(1\d{9}|3\d{8}|5\d{8}|7\d{8}|8\d{8}|9\d{8})$/.test(phone);
+}
+
+export function isURL(str) {
+    var urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+    var url = new RegExp(urlRegex, 'i');
+    return str.length < 2083 && url.test(str);
 }
